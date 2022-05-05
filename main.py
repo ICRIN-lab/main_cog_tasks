aa2a5f23f08ae7801c2f65f2583d7d99bdf2a825
mselleki <mselleki98@gmail.com>
2022-05-05 13:52:13 +0200

    add timestamps

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import os
+import time
 
+ts = time.time()
 os.chdir('../seven_diff/')
 os.system('python seven_diff.py')
 
+ts_1 = time.time()
 os.chdir('../lucifer')
 os.system('python lucifer.py')
 
+ts_2 = time.time()
 os.chdir('../where_is_tockie')
 os.system('python where_is_tockie.py')
 
+ts_3 = time.time()
 os.chdir('../symetry')
 os.system('python symetry.py')
+
+ts_end = time.time()
